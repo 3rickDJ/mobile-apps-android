624f5ee0978de4dd001256b0df9ed217ae525263
Erick De Jesus <94492584+3rickDJ@users.noreply.github.com>
2024-12-06 13:31:12 -0600

    retrieve items from serviciosdigitalsplus

--- 15proyecto/app/src/main/java/com/example/a15proyecto/MostrarActivity.java
+++ 15proyecto/app/src/main/java/com/example/a15proyecto/MostrarActivity.java
@@ -1,6 +1,7 @@
 package com.example.a15proyecto;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +11,24 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class MostrarActivity extends AppCompatActivity {
 
     RecyclerView rc;
-    ArrayList<Articulos>lista;
+    ArrayList<Articulos> lista;
     RequestQueue requestQueue;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +41,59 @@ public class MostrarActivity extends AppCompatActivity {
             return insets;
         });
 
-        lista = new ArrayList<Articulos>();
+        // Inicializar RecyclerView y lista
+        lista = new ArrayList<>();
+        rc = findViewById(R.id.RecyclerView);
+        rc.setLayoutManager(new GridLayoutManager(this, 1));
 
-        for(int i=0; i<10; i++){
-            Articulos a = new Articulos( "a","i ","12","11/12/2024");
-            lista.add(a);
-        }
-        rc = (RecyclerView) findViewById(R.id.RecyclerView);
-        rc.setLayoutManager(new GridLayoutManager(this,1));
+        // Configurar Volley
+        requestQueue = Volley.newRequestQueue(this);
 
-        adaptar ad = new adaptar(lista);
-        rc.setAdapter(ad);
+        // Llamar al método para cargar datos
+        cargarDatos();
+    }
+
+    private void cargarDatos() {
+        String url = "https://serviciosdigitalesplus.com/distribuida2024/procesos.php?tipo=4";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray array = response.getJSONArray("array");
+                            for (int i = 0; i < array.length(); i++) {
+                                JSONObject obj = array.getJSONObject(i);
+
+                                // Crear objeto Articulos
+                                String id = obj.getString("id");
+                                String nom = obj.getString("nom");
+                                String costo = obj.getString("costo");
+                                String fecha = obj.getString("fecha");
+
+                                Articulos articulo = new Articulos(id, nom, costo, fecha);
+                                lista.add(articulo);
+                            }
+
+                            // Configurar el adaptador con los datos obtenidos
+                            adaptar ad = new adaptar(lista);
+                            rc.setAdapter(ad);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("Volley", "Error al parsear JSON: " + e.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley", "Error en la petición: " + error.getMessage());
+                    }
+                });
+
+        // Agregar la petición a la cola
+        requestQueue.add(jsonObjectRequest);
     }
 }
